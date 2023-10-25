c238411b416ebd50c7c6bf99c7f0f16936f4159a
Pulkit <kpulkit15234@gmail.com>
2023-10-25 17:05:41 +0530

    stling edit route

--- 47. Major Project/index.js
+++ 47. Major Project/index.js
@@ -8,6 +8,14 @@ const method_override = require("method-override");
 
 const listingRouter = require("./controllers/listingRoutes.js");
 
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(method_override("_method"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", "ejs");
+app.engine("ejs", ejsMate)
+
 const main = async () => {
   await mongoose.connect(MONGO_URL);
 };
@@ -20,20 +28,12 @@ main()
     console.log(err);
   });
 
-// app.use(express.static(path.join(__dirname, "/public")));
-app.use(express.static('C:/Users/ASUS/Desktop/Courses/Delta/Codes n all/47. Major Project/public'));
-app.use(method_override("_method"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.engine("ejs",ejsMate)
+
 
 app.use("/listings", listingRouter);
 app.get('/new', async (req, res) => {
   res.render("listings/newListing")
 })
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 
 const port = 3003;
 app.listen(port, () => {
